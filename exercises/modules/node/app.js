d1ea7470235f77ba6034401ac31652a871388cdd
Kyle Winkler <kyle@caxy.com>
2021-06-29 11:33:50 -0500

    nodify the api mock

---
+++ exercises/modules/node/app.js
@@ -0,0 +1,32 @@
+const { getUserById, getPostsForUser } = require('./api');
+
+const showPostsForCurrentUser = (userId, cb) => {
+  getPostsForUser(userId, posts => {
+    const postTemplates = posts.map(post => {
+      return `
+      <div class="post">
+        ${post.title}
+        ${post.body}
+        ${post.createdBy}
+      </div>`
+    })
+    cb(postTemplates)
+  })
+}
+
+const showUserProfile = (userId, cb) => {
+  getUserById(userId, user => {
+    const profile = `
+      <div>
+        ${user.name}
+      </div>
+    `
+    cb(user)
+  })
+}
+
+module.exports = {
+  showUserProfile,
+  showPostsForCurrentUser
+}
+
